5cc0f53db8192b0bd3505edc4594252a6bc3f381
sanjayashok1 <ghashok20@gmail.com>
2024-07-27 15:51:11 +0500

    assignment 5

---
+++ main.dart
@@ -0,0 +1,22 @@
+void main (){
+    //question no 1
+    String name1 ="Shahzeb";
+    String name2 ="Younus";
+    String name3 ="Arham";
+    String name4 ="Abdullah";
+    List nameList = ["Shahzeb","Younus","Arham","Abdullah"];
+    print (nameList);
+    //question no 2
+    String day1 ="monday";
+    String day2 = "tuesday";
+    String day3 = "wednesday";
+    String day4 = "thursday";
+    String day5 ="friday";
+    String day6 = "saturday";
+    String day7 = "sunday";
+    List daysOfWeekList =["monay", "tuesday","wednesday","thursady","friday","saturday","sunday"];
+    print (daysOfWeekList[daysOfWeekList.length-1]);
+    //question no 3
+      List studentData = ["Sanjay", "XII","322952", "A", "76"];
+      print (studentData);
+}
